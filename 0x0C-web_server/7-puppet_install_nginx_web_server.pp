063dd41c2bd9edee0b3578ba66901ed8d5e35fa2
Dev-wonderful <adeonederful20@gmail.com>
2023-06-27 23:02:01 +0000

    puppet configure for ubuntu

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,30 +2,30 @@
 
 # run update before installing package
 exec {'update':
-  path    => '/usr/bin:/bin',
-  command => 'sudo apt-get -y update'
+  provider => shell,
+  command  => 'sudo apt-get -y update'
 }
 
 # install package
 exec {'install nginx':
-  command => 'sudo apt-get -y nginx',
-  path    => '/usr/bin:/bin'
+  provider => shell,
+  command  => 'sudo apt-get -y nginx',
 }
 
 # redirect permanently
 exec {'redirect':
-  command => 'sed -i "/server_name _;/a \\n\trewrite ^\/redirect_me \/ permanent;" /etc/nginx/sites-available/default',
-  path    => ['/usr/bin', '/bin']
+  provider => shell,
+  command  => 'sudo sed -i "/server_name _;/a \\n\trewrite ^\/redirect_me \/ permanent;" /etc/nginx/sites-available/default',
 }
 
 # Create default index file
 exec {'create index file':
-  command => 'echo "Hello World!" > /var/www/html/index.html',
-  path    => '/usr/bin:/bin'
+  provider => shell,
+  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
 }
 
 # start nginx
 exec {'start':
-  command => 'service nginx start',
-  path    => '/usr/bin:/bin'
+  provider => shell,
+  command  => 'sudo service nginx start',
 }
