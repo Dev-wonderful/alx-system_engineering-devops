8f489abfe6b096b9ede1ad6081a479850b670096
Dev-wonderful <adeonederful20@gmail.com>
2023-07-04 00:52:54 +0100

    2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,32 +1,36 @@
 # configure a web server to add a custom header
 
 # updating package repository
-exec {'update':
+exec {'a:update':
   provider => shell,
-  command  => 'apt -y update',
+  command  => 'sudo apt -y update',
+  before   => Exec['b:install nginx']
 }
 
 # install package
-exec {'install nginx':
+exec {'b:install nginx':
   provider => shell,
-  path     => '/usr/bin:/bin',
-  command  => 'apt-get -y install nginx',
+  command  => 'sudo apt -y install nginx',
+  require  => Exec['a:update']
 }
 
 # Create default index file
-exec {'create index file':
+exec {'c:create index file':
   provider => shell,
   command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+  require  => Exec['b:install nginx']
 }
 
 # response header
-exec{'custom-header':
+exec{'d:custom-header':
   provider => shell,
   command  => "sudo sed -i '/server_name _;/a \\\tadd_header X-Served-By ${hostname};' /etc/nginx/sites-available/default",
+  require  => Exec['c:create index file']
 }
 
 # restart nginx after config  update
-exec {'restart':
+exec {'e:restart':
   provider => shell,
   command  => 'sudo service nginx restart',
+  require  => Exec['d:custom-header']
 }
