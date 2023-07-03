1d0150764d0728eed79f2cc9d9cafa9c200128d8
Dev-wonderful <adeonederful20@gmail.com>
2023-07-03 22:26:52 +0000

    puppet manifest

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,12 +1,32 @@
 # configure a web server to add a custom header
 
+# updating package repository
+exec {'update':
+  provider => shell,
+  command  => 'apt -y update',
+}
+
+# install package
+exec {'install nginx':
+  provider => shell,
+  path     => '/usr/bin:/bin',
+  command  => 'apt-get -y install nginx',
+}
+
+# Create default index file
+exec {'create index file':
+  provider => shell,
+  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+}
+
+# response header
 exec{'custom-header':
   provider => shell,
   command  => "sudo sed -i '/server_name _;/a \\\tadd_header X-Served-By ${hostname};' /etc/nginx/sites-available/default",
 }
 
-# restart nginx
-exec {'start':
+# restart nginx after config  update
+exec {'restart':
   provider => shell,
-  command  => 'sudo service nginx start',
+  command  => 'sudo service nginx restart',
 }
