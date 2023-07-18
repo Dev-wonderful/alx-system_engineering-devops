674348f6064e9da2ef13df5649c90aa544f272d1
Dev-wonderful <adeonederful20@gmail.com>
2023-07-18 16:36:37 +0000

    follow_up syntax fix

--- 0x14-mysql/follow_up.sql
+++ 0x14-mysql/follow_up.sql
@@ -1,10 +1,11 @@
 -- create database and table and grant user select priivilege on this table
 CREATE DATABASE IF NOT EXISTS `tyrell_corp`;
 USE `tyrell_corp`;
+-- no commas after the last datatype, otherwise error
 CREATE TABLE IF NOT EXISTS `nexus6` (
 	`id` INT,
-	`name` VARCHAR(256),
+	`name` VARCHAR(256)
 );
 INSERT INTO `nexus6` (`id`, `name`)
 VALUES (1, "Leon");
-GRANT SELECT PRIVILEGES ON `tyrell_corp`.`nexus6` TO 'holberton_user'@'localhost';
+GRANT SELECT ON `tyrell_corp`.`nexus6` TO 'holberton_user'@'localhost';
