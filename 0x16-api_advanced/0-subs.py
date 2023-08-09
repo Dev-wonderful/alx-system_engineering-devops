879d61496999c78823c8fd52c8a67884b30b8898
Dev-wonderful <adeonderful20@gmail.com>
2023-08-09 01:39:56 +0100

    fetch number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,13 +3,16 @@
 import requests
 
 
-def number_of_subscribers(subreddit):
+def number_of_subscribers(subreddit) -> int:
     """returns the number of subs for a subreddit
     Args:
         subreddit(string): the fetch focus
     """
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    result = requests.get(url)
+    result = requests.get(url, allow_redirects=False)
     about = result.json()
-    subs = about.get('data').get('subscribers')
+    if about is None:
+        return 0
+    about = about.get('data')
+    subs = about.get('subscribers')
     return subs
