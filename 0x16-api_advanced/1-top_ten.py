4eb41512b553b8377501d1ed3f001ac5f7f76686
Dev-wonderful <adeonderful20@gmail.com>
2023-08-09 03:29:11 +0100

    print top ten posts 3

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-"""fetch the number of subscribers for a particular subreddit"""
+"""fetch the top 10 posts for a particular subreddit"""
 import requests
 
 
 def top_ten(subreddit):
-    """returns the number of subs for a subreddit
+    """prints the top 10 posts title for a subreddit
     Args:
         subreddit(string): the fetch focus
     """
     header = {
         "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
     }
-    url = f'https://www.reddit.com/r/{subreddit}/top.json?limit=10'
-    result = requests.get(url, allow_redirects=False, headers=header)
+    params = {
+        "limit": 10
+    }
+    url = f'https://www.reddit.com/r/{subreddit}/top.json'
+    result = requests.get(url, allow_redirects=False,
+                          headers=header, params=params)
     top_posts = result.json().get('data', None)
     if top_posts is None:
         print(None)
