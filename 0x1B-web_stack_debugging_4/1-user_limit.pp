5672b9ef782772d006355dc0994ae43596507bb8
Dev-wonderful <adeonederful20@gmail.com>
2023-08-17 18:55:39 +0100

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,11 @@
 # modify user limit
-exec {'holberton user limit mod':
+exec {'holberton hard limit mod':
   provider => 'shell',
   command  => 'sed -i "s/hard nofile 5/hard nofile 4096/" /etc/security/limits.conf'
+}
+
+# soft limit
+exec {'holberton soft limit mod':
+  provider => 'shell',
+  command  => 'sed -i "s/soft nofile 4/soft nofile 100/" /etc/security/limits.conf'
 }
